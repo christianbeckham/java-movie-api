26c5869e0d8f4f0aaf69c4c65bd5fb7425d3f6e8
Christian Beckham <urenacb22@gmail.com>
2022-11-15 13:01:43 -0600

    feat: added a get request route and created the findAllMovies method

--- src/main/java/com/dcc/movieapi/controller/MovieController.java
+++ src/main/java/com/dcc/movieapi/controller/MovieController.java
@@ -3,9 +3,12 @@ package com.dcc.movieapi.controller;
 import com.dcc.movieapi.data.Movie;
 import com.dcc.movieapi.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
@@ -16,5 +19,10 @@ public class MovieController {
     @PostMapping("/addMovie")
     public Movie addMovie(@RequestBody Movie movie) {
         return movieService.saveMovie(movie);
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> findAllMovies() {
+        return movieService.getAllMovies();
     }
 }
